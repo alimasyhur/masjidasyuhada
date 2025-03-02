6c85f812cb131b9e33c23a4958b4e10184cae50d
Ali Masyhur <jegrag4ever@gmail.com>
2025-03-02 23:46:08 +0700

    feat(relawan): hide menu relawan

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -14,7 +14,7 @@
             <h1><strong>Program & Events</strong></h1>
             <br>
             <p>Berikut ini adalah daftar Program dan Events Ramadhan Fest Masjid Sejuta Pemuda. Untuk lebih detail dan mendaftar eventnya bisa klik
-                <a href="/event" class="btn btn-primary">menu event</a>
+                <br><a href="/event" class="btn btn-primary btn-lg">menu event</a>
             </p>
         </div>
         <img src="{{ asset('assets/img/ramadhanfest3.jpeg') }}" alt="Image" class="img-thumbnail" width="100%">
